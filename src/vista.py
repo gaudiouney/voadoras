5f70e6faac5caee89d5836f938c3428f4e213f0d
gaudiouchoaney <gaudicorj@gmail.com>
2014-05-20 09:54:14 -0300

    Tela inicial (teste) deque onclick voa

--- src/vista.py
+++ src/vista.py
@@ -1,6 +1,38 @@
 __author__ = 'gaudiouney'
+FACE = 'http://misslittlecherry.files.wordpress.com/2009/12/1213276673857798001xo0.jpg'
+CH = CV = 50
+class Carta:
+    def __init__(self, html, xy, deque):
+        x, y = self.pos = xy
+        self.deque = deque
+        ct = self.e_carta = html.IMG(src=FACE, width=CH, heigth=CV)
+        ct.style.position = "absolute"
+        ct.style.left, ct.style.top = xy
+        x = x / 5
+        ct.style.transition = "left 2s linear %fs, top 2s linear %fs" % (x, x)
+        deque <= ct
+        ct.onclick = self.voa
+    def voa(self, evento):
+        self.deque.voa()
 
+    def voar(self, delta):
+        dx, dy = delta
+        x, y = self.pos
+        xy = self.pos = x + dx, y + dy
+        ct = self.e_carta
+        ct.style.left, ct.style.top = xy
+
+class Deque:
+    def __init__(self, html, tela):
+        self.tela = tela
+        self.deque = [Carta(html, (x*4, 10), self) for x in range(10)]
+
+    def voa(self):
+        [carta.voar((100, 100)) for carta in self.deque]
+    def __le__(self, other):
+        self.tela <= other
 def tabuleiro(tela, html):
+
     tabul = html.DIV()
     tela <= tabul
 
@@ -10,11 +42,13 @@ def embaralha(tela, html):
 def voa(tela, html):
     pass
 
-def main(html, doc):
-    tela = doc["main"]
-    #html = gui.html
-    splash = html.DIV("VOADORAS")
-    tela <= splash
-    tabuleiro(tela, html)
-    embaralha(tela, html)
-    voa(tela, html)
+ def main(html, doc):
+     tela = doc["main"]
+     #html = gui.html
+     splash = html.DIV("VOADORAS")
+     tela <= splash
+     tela <= splash
+     #tabuleiro(tela, html)
+     #embaralha(tela, html)
+     #voa(tela, html)
+     deque = Deque(html, tela)
