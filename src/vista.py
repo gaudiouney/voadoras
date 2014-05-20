d1e34df77aea0abed4c73af211d0a63d3d9875e1
gaudiouchoaney <gaudicorj@gmail.com>
2014-05-20 08:24:13 -0300

    Tela inicial (teste)

--- src/vista.py
+++ src/vista.py
@@ -1,8 +1,20 @@
 __author__ = 'gaudiouney'
 
+def tabuleio(tela, html):
+    tabul = html.DIV()
+    tela <= tabul
+
+def embaralha(tela, html):
+    pass
+
+def voa(tela, html):
+    pass
 
 def main(html, doc):
     tela = doc["main"]
     #html = gui.html
     splash = html.DIV("VOADORAS")
     tela <= splash
+    tabuleiro(tela, html)
+    embaralha(tela, html)
+    voa(tela, html)
